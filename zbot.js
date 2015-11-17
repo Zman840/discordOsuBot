dcd1ab0eb04e7ffc74a3edfcf99a71d895a7c04f
abalabahaha <jerryxu23@gmail.com>
2015-11-17 00:04:14 -0800

    Started things

--- zbot.js
+++ zbot.js
@@ -1,32 +1,128 @@
+var Discord = require("discord.js");
 var fs = require('fs');
+var Loki = require("lokijs");
 var util = require('util');
 
-//log in debug.log
 var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
-var log_stdout = process.stdout;
 console.log = function(d) { //
-  log_file.write(util.format(d) + '\n');
-  log_stdout.write(util.format(d) + '\n');
+    log_file.write(util.format(d) + '\n');
+    process.stdout.write(util.format(d) + '\n');
 };
 
+var config = require("./config.json");
+var adminCommands = ['setpoints'];
 
-//the bot itself
-var Discord = require("discord.js");
-var zbot = new Discord.Client();
-
-//username/password auth
-var AuthDetails = require("./auth.json");
-
-//json handler to write/read
-var jsonfile = require("jsonfile");
-
-
-
-zbot.on("ready", function () {
-	console.log("Ready to begin! Serving in " + zbot.channels.length + " channels");
+var users = null;
+var db = new Loki('data.json', {
+	autoload: true,
+	autoloadCallback: () => {
+		if(!(users = db.getCollection('users'))) {
+			users = db.addCollection('users');
+		}
+		users.findOrCreate = (query) => {
+			var user = users.findOne(query);
+			if(!user) {
+				user = {
+					userid: query.userid,
+					points: 0,
+					pointSpread: {},
+					infractions: 0,
+					infractionSpread: {}
+				};
+				users.insert(user);
+				saveDatabase();
+			}
+			return user;
+		};
+		console.log('Database loaded!');
+	}
 });
 
-//id is like that for object finding to find properly as an idea. Planning to use jsonfile as a handler for that
+processMessage = (msg) => {
+	users.findOrCreate({userid: msg.author.id});
+	if(msg.content.startsWith(config.prefix)) {
+		var args = msg.content.split(" ");
+		var cmd = args.shift().substring(config.prefix.length).toLowerCase();
+		if(adminCommands.indexOf(cmd) !== -1) {
+			if(!hasAdmin(msg.channel.server, msg.author)) {
+				console.log(`${msg.author.username} tried to use ${cmd}!`);
+				return;
+			}
+			console.log("Authenticated "  + msg.author.username);
+			if(cmd === "setpoints") {
+				if(args.length < 2 || !args[0].match(/^<@[0-9]{15,25}>$/) || isNaN(args[1])) {
+					return bot.sendMessage(msg, "You're typing it wrong.");
+				}
+				var user = users.findOrCreate({userid: args[0].substring(2, args[0].length - 1)});
+				user.points = +args[1];
+				users.update(user);
+				saveDatabase();
+				bot.sendMessage(msg, `${msg.author.username} now has ${user.points} points!`);
+			}
+		}
+		else {
+			if(cmd === "points") {
+				var target = msg.author.id;
+				if(args.length >= 1 && (reg = args[0].match(/^<@[0-9]{15,25}>$/))) {
+					target = args[0].substring(2, args[0].length - 1);
+				}
+				var user = users.findOne({userid: target});
+				if(!user) {
+					return bot.sendMessage(msg, "User not found!");
+				}
+				else {
+					bot.sendMessage(msg, `${msg.author.username} has ${user.points} points!`);
+				}
+			}
+		}
+	}
+	else if(msg.content.match(/^<@[0-9]{15,25}> *\+\+/)) {
+		var target = msg.content.substring(2, msg.content.indexOf('>'));
+		var user = users.findOne({userid: target});
+		if(!user) {
+			return bot.sendMessage(msg, "User not found!");
+		}
+		user.points++;
+		users.update(user);
+		saveDatabase();
+		bot.sendMessage(msg, `${msg.author.username} gave 1 point to ${bot.users.get('id', target)}!`);
+	}
+};
+
+hasAdmin = (server, user) => {
+	var roles = server.rolesOf(user);
+	for(i in roles) {
+		if(roles[i].name.match(/(admin|owner)/i)) {
+			return true;
+		}
+	}
+	return false;
+};
+
+saveDatabase = () => {
+	db.saveDatabase();
+	console.log('Saved database!');
+};
+
+var bot = new Discord.Client();
+
+bot.on("ready", () => {
+	console.log("Ready to begin! Serving in " + bot.channels.length + " channels");
+});
+
+bot.on("message", (message) => {
+	processMessage(message);
+});
+
+bot.on("disconnected", () => {
+	console.log("Disconnected from Discord!");
+	process.exit(1);
+});
+
+bot.login(config.auth.email, config.auth.pass);
+
+/*
+
 var pointsSystem = [{
     'id12345678': {
     	"name": "Zman840",
@@ -53,71 +149,4 @@ var pointsSystem = [{
     	"lastMessage": 144179
     }
 }];
-
-zbot.on("message", function(message) {
-	if (message.client)
-	//console.log(message)
-	console.log(message);
-	//respond under 2 conditions: You have an administrative role && you have
-	var serverID = zbot.getServer("id", message.channel.serverID),
-	serverRoles = [],
-	serverRoleName = [],
-	Admincommands = ["!zPoints", "!zBeatmap"],
-	messageArray = message.split(" ");
-	//beatmap command expressions planned:
-	//Administrators / event planners:
-		//beatmap link
-    ///regular users
-		//beatmap upload
-
-	for (serverRole in serverID.roles) {
-		var name = serverID["roles"][serverRole]["name"],
-		id = serverID["roles"][serverRole]["id"],
-		roleID = {name: name, id: id};
-
-		serverRoles.push(id);
-		serverRoleName.push(name);
-		//console.log(roleID)
-	}
-
-	if (roleCheck(message.author.rawRoles, serverRoles, serverRoleName, message.content)) {
-		console.log("role access response Allowed");
-	}
-	/*
-	    if (message.content === "ping")
-	        zbot.reply(message, "pong");
-	    else if (message.content === "!zTest")
-	    	zbot.reply(message, "test success");
-	    else
-	    	zbot.sendMessage(message, "test Failed");
-
-	*/
-});
-
-zbot.on("disconnected", function () {
-
-	console.log("disconnected from discord!");
-	process.exit(1); //exit node.js with an error
-	
-});
-
-//create functions for many things, including role check
-function AdminCheck (authorRoleArray, serverRoleArray, serverRoleName) {
-
-	//match role indexes with names and set it as its own array
-	var names = [], role;
-	for (id in authorRoleArray) {
-		role = serverRoleArray.indexOf(authorRoleArray[id]);
-		names.push(serverRoleName[role]);
-	}
-	console.log(names);
-
-
-	if (names.indexOf("Admin") !== -1 || names.indexOf("Owner") !== -1) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-zbot.login(AuthDetails.email, AuthDetails.password);
+*/
